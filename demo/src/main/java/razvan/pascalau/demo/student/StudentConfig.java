5e04d4a2799643ef380ac9d150cec351c2ec4491
razvanpascalu21 <razvanpascalau21@yahoo.com>
2022-07-28 09:12:28 +0300

    Implement delete put post Spring

--- demo/src/main/java/razvan/pascalau/demo/student/StudentConfig.java
+++ demo/src/main/java/razvan/pascalau/demo/student/StudentConfig.java
@@ -8,13 +8,16 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import static java.time.Month.DECEMBER;
+import static java.time.Month.MARCH;
+
 @Configuration
 public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository){
         return args -> {
-            Student maria=new Student("Maria","maria.jamila@yahoo.com", LocalDate.of(2000, Month.DECEMBER,18));
-            Student alex=new Student("Alex","alex@yahoo.com", LocalDate.of(2001, Month.MARCH,18));
+            Student maria=new Student("Maria","maria.jamila@yahoo.com", LocalDate.of(2000, DECEMBER,18));
+            Student alex=new Student("Alex","alex@yahoo.com", LocalDate.of(2004, MARCH,18));
             studentRepository.saveAll(List.of(alex,maria));
         };
     }
--- demo/src/main/java/razvan/pascalau/demo/student/StudentController.java
+++ demo/src/main/java/razvan/pascalau/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package razvan.pascalau.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,21 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents(){
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long id){
+        studentService.deleteStudent(id);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long id,@RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email){
+        studentService.updateStudent(id,name,email);
     }
 }
